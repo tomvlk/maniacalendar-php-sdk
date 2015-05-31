cf12befcce11fcb448422346d8bf6b5b8448288e
Tom Valk <tomvalk@gmail.com>
2015-05-31 14:39:56 +0200

    Adding documentation for event and date methods

--- src/maniacalendar/Dates.php
+++ src/maniacalendar/Dates.php
@@ -8,6 +8,16 @@ class Dates extends Client {
         parent::__construct($key);
     }
     
+    /**
+     * Get dates for an event
+     * 
+     * @link http://api.maniacalendar.com/doc/dates Documentation for API method.
+     * @param int $eventid Give the EventID
+     * @param int $from Get from UTC Timestamp, default (null) is today 00:00.
+     * @param int $limit Limit the dates, default (null) is 50.
+     * @param boolean $ids Also show Date IDs. Can be usefull when editing.
+     * @return array Array with objects.
+     */
     public function getDates($eventid, $from = null, $limit = 50, $ids = false) {
         return $this->execute('GET', 'dates/' . intval($eventid), array('from' => $from, 'limit' => $limit, 'ids' => $ids));
     }
--- src/maniacalendar/Event.php
+++ src/maniacalendar/Event.php
@@ -8,7 +8,31 @@ class Event extends Client {
         parent::__construct($key);
     }
     
+    /**
+     * Get events from API,
+     * 
+     * @link http://api.maniacalendar.com/doc/event Documentation for API method.
+     * @param string $game can be either 'trackmania' or 'shootmania'. Null for both.
+     * @param int $style Give the unique id for filtering on style.
+     * @param string $title Give a titleid (string) for filtering on title. default null, no filtering on title.
+     * @param int $start Filter on events with dates from this UTC Timestamp, default today at 00:00.
+     * @param int $end Filter on events with dates ending before UTC Timestamp, default none (null).
+     * @param type $limit Limit the results, default 20.
+     * @param type $offset Start from an offset (use when filtering and want pages)
+     * @return array Array with objects.
+     */
     public function getEvents($game = null, $style = null, $title = null, $start = null, $end = null, $limit = 20, $offset = 0) {
         return $this->execute('GET', 'events', ['game' => $game, 'style' => $style, 'title' => $title, 'start' => $start, 'end' => $end, 'limit' => $limit, 'offset' => $offset]);
+    }
+    
+    /**
+     * Get details for one event.
+     * 
+     * @link http://api.maniacalendar.com/doc/event Documentation for API method.
+     * @param int $eventid Give the EventID.
+     * @return object
+     */
+    public function getEventDetails($eventid) {
+        return $this->execute('GET', 'events/' . intval($eventid));
     }
 }
